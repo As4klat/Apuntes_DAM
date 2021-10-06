0fccade14473c6952e0288601eac4258b9d9cc97
Asklat <dkasalgama@gmail.com>
2021-10-06 10:23:50 +0200

    nose

--- Programacion de servicios y procesos (PSPRO)/Primer bloque/Paneles/src/main/java/PanelB.java
+++ Programacion de servicios y procesos (PSPRO)/Primer bloque/Paneles/src/main/java/PanelB.java
@@ -5,7 +5,10 @@
  */
 
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 /**
@@ -13,12 +16,26 @@ import javax.swing.JPanel;
  * @author Alejandro
  */
 public class PanelB extends JPanel {
-    public PanelB() {
+    private JFrame ventana;
+    
+    public PanelB(Ventana ventana) {
         super();
+        
+        this.ventana = ventana;
+        
         this.setLayout(new FlowLayout());
         
-        JButton btnIrBa = new JButton("Ir a panel A");
-        this.add(btnIrBa);
+        JButton btnIraPanelA = new JButton("Ir a panel A");
+        this.add(btnIraPanelA);
         
+        btnIraPanelA.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                ventana.panelActivo.setVisible(false);
+                ventana.panelActivo = new PanelA(ventana);
+                ventana.getContentPane().add(ventana.panelActivo);
+            }
+ 
+        });    
     }
 }
---
+++ Programacion de servicios y procesos (PSPRO)/Primer bloque/VentanasMoviendo/src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Ventana v = new Ventana();
+        v.setVisible(true);
+    }
+}
