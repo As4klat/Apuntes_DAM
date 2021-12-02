7a636096b7aaf7aa6ca7a5936c2ccf62a159391b
Asklat <dkasalgama@gmail.com>
2021-12-02 17:26:13 +0100

    angular done, interfaces

--- Programacion multimedia y dispositivos moviles (PMDMO)/Primer bloque/Angular/blog/src/app/Componentes/modo-oscuro/modo-oscuro.component.ts
+++ Programacion multimedia y dispositivos moviles (PMDMO)/Primer bloque/Angular/blog/src/app/Componentes/modo-oscuro/modo-oscuro.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-modo-oscuro',
   templateUrl: './modo-oscuro.component.html',
   styleUrls: ['./modo-oscuro.component.css']
 })
-export class ModoOscuroComponent implements OnInit {
+export class ModoOscuroComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  oscuro: boolean;
+  constructor() { 
+    this.oscuro = false;
   }
 
+  @Output() _toggleTheme = new EventEmitter<boolean>();
+  toggleTheme(){
+    this.oscuro = !this.oscuro;
+    this._toggleTheme.emit(this.oscuro);
+  }
 }
