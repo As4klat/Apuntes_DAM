762229d507c08bef70314139011d6816e72d9318
Asklat <algama161098@gmail.com>
2022-01-13 01:07:12 +0100

    Actividad 21 interfaces

--- Desarrollo de interfaces (DEINT)/Segundo bloque/Tema 7/Ejercicios/Actividad21/Actividad21-2/MainWindow.xaml.cs
+++ Desarrollo de interfaces (DEINT)/Segundo bloque/Tema 7/Ejercicios/Actividad21/Actividad21-2/MainWindow.xaml.cs
@@ -23,42 +23,81 @@ namespace Actividad21_2
         public MainWindow()
         {
             InitializeComponent();
+            /*
+             * 120
+             * 2 - 1 - 1 - 1 - 2
+             * 
+             */
         }
 
+        // Boton del menu 'Combat Skills'
         private void menuCombatSkills_MouseEnter(object sender, MouseEventArgs e)
         {
             menuCombatSkills.Foreground = new SolidColorBrush(Colors.White);
         }
         private void menuCombatSkills_MouseLeave(object sender, MouseEventArgs e)
         {
-            menuCombatSkills.Foreground = new SolidColorBrush(Color.FromRgb(255, 73, 73));
+            Color color = (Color)ColorConverter.ConvertFromString("#FFFF4949");
+            menuCombatSkills.Foreground = new SolidColorBrush(color);
         }
 
+        // Boton del menu 'Signs Skills'
         private void menuSignsSkills_MouseEnter(object sender, MouseEventArgs e)
         {
-
+            menuSignsSkills.Foreground = new SolidColorBrush(Colors.White);
         }
 
+        private void menuSignsSkills_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Color color = (Color)ColorConverter.ConvertFromString("#FF5C55B1");
+            menuSignsSkills.Foreground = new SolidColorBrush(color);
+        }
+
+        // Boton del menu 'Alchemy Skills'
         private void menuAlchemySkills_MouseEnter(object sender, MouseEventArgs e)
         {
-
+            menuAlchemySkills.Foreground = new SolidColorBrush(Colors.White);
         }
 
+        private void menuAlchemySkills_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Color color = (Color)ColorConverter.ConvertFromString("#FF52725B");
+            menuAlchemySkills.Foreground = new SolidColorBrush(color);
+        }
+
+        // Boton del menu 'General Skills'
         private void menuGeneralSkills_MouseEnter(object sender, MouseEventArgs e)
         {
-
+            menuGeneralSkills.Foreground = new SolidColorBrush(Colors.White);
         }
 
+        private void menuGeneralSkills_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Color color = (Color)ColorConverter.ConvertFromString("#FFAC8C66");
+            menuGeneralSkills.Foreground = new SolidColorBrush(color);
+        }
+
+        // Boton del menu 'Mutagens'
         private void menuMutagens_MouseEnter(object sender, MouseEventArgs e)
         {
-
+            menuMutagens.Foreground = new SolidColorBrush(Colors.White);
         }
 
+        private void menuMutagens_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Color color = (Color)ColorConverter.ConvertFromString("#FF6D859D");
+            menuMutagens.Foreground = new SolidColorBrush(color);
+        }
+
+        // Boton del menu 'Mutations'
         private void menuMutations_MouseEnter(object sender, MouseEventArgs e)
         {
-
+            menuMutations.Foreground = new SolidColorBrush(Colors.White);
         }
-
-        
+        private void menuMutations_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Color color = (Color)ColorConverter.ConvertFromString("#FF948464");
+            menuMutations.Foreground = new SolidColorBrush(color);
+        }
     }
 }
