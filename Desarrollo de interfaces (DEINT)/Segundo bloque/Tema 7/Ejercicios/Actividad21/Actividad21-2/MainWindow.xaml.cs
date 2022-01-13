1ef6e9df61635b0b1dcb4f185d9c8a7377cb0155
Asklat <dkasalgama@gmail.com>
2022-01-13 12:23:57 +0100

    Ineterfaces A21 pruebasgrid

--- Desarrollo de interfaces (DEINT)/Segundo bloque/Tema 7/Ejercicios/Actividad21/Actividad21-2/MainWindow.xaml.cs
+++ Desarrollo de interfaces (DEINT)/Segundo bloque/Tema 7/Ejercicios/Actividad21/Actividad21-2/MainWindow.xaml.cs
@@ -23,81 +23,70 @@ namespace Actividad21_2
         public MainWindow()
         {
             InitializeComponent();
-            /*
-             * 120
-             * 2 - 1 - 1 - 1 - 2
-             * 
-             */
         }
 
         // Boton del menu 'Combat Skills'
         private void menuCombatSkills_MouseEnter(object sender, MouseEventArgs e)
         {
-            menuCombatSkills.Foreground = new SolidColorBrush(Colors.White);
+            menuCombatSkills.Foreground.Opacity = 1.0;
         }
         private void menuCombatSkills_MouseLeave(object sender, MouseEventArgs e)
         {
-            Color color = (Color)ColorConverter.ConvertFromString("#FFFF4949");
-            menuCombatSkills.Foreground = new SolidColorBrush(color);
+            menuCombatSkills.Foreground.Opacity = 0.2;
         }
 
         // Boton del menu 'Signs Skills'
         private void menuSignsSkills_MouseEnter(object sender, MouseEventArgs e)
         {
-            menuSignsSkills.Foreground = new SolidColorBrush(Colors.White);
+            menuSignsSkills.Foreground.Opacity = 1.0;
         }
 
         private void menuSignsSkills_MouseLeave(object sender, MouseEventArgs e)
         {
-            Color color = (Color)ColorConverter.ConvertFromString("#FF5C55B1");
-            menuSignsSkills.Foreground = new SolidColorBrush(color);
+            menuSignsSkills.Foreground.Opacity = 0.2;
         }
 
         // Boton del menu 'Alchemy Skills'
         private void menuAlchemySkills_MouseEnter(object sender, MouseEventArgs e)
         {
-            menuAlchemySkills.Foreground = new SolidColorBrush(Colors.White);
+            menuAlchemySkills.Foreground.Opacity = 1.0;
         }
 
         private void menuAlchemySkills_MouseLeave(object sender, MouseEventArgs e)
         {
-            Color color = (Color)ColorConverter.ConvertFromString("#FF52725B");
-            menuAlchemySkills.Foreground = new SolidColorBrush(color);
+            menuAlchemySkills.Foreground.Opacity = 0.2;
         }
 
         // Boton del menu 'General Skills'
         private void menuGeneralSkills_MouseEnter(object sender, MouseEventArgs e)
         {
-            menuGeneralSkills.Foreground = new SolidColorBrush(Colors.White);
+            menuGeneralSkills.Foreground.Opacity = 1.0;
         }
 
         private void menuGeneralSkills_MouseLeave(object sender, MouseEventArgs e)
         {
-            Color color = (Color)ColorConverter.ConvertFromString("#FFAC8C66");
-            menuGeneralSkills.Foreground = new SolidColorBrush(color);
+            menuGeneralSkills.Foreground.Opacity = 0.2;
         }
 
         // Boton del menu 'Mutagens'
         private void menuMutagens_MouseEnter(object sender, MouseEventArgs e)
         {
-            menuMutagens.Foreground = new SolidColorBrush(Colors.White);
+            menuMutagens.Foreground.Opacity = 1.0;
         }
 
         private void menuMutagens_MouseLeave(object sender, MouseEventArgs e)
         {
-            Color color = (Color)ColorConverter.ConvertFromString("#FF6D859D");
-            menuMutagens.Foreground = new SolidColorBrush(color);
+            menuMutagens.Foreground.Opacity = 0.2;
         }
 
         // Boton del menu 'Mutations'
         private void menuMutations_MouseEnter(object sender, MouseEventArgs e)
         {
-            menuMutations.Foreground = new SolidColorBrush(Colors.White);
+            menuMutations.Foreground.Opacity = 1.0;
         }
         private void menuMutations_MouseLeave(object sender, MouseEventArgs e)
         {
-            Color color = (Color)ColorConverter.ConvertFromString("#FF948464");
-            menuMutations.Foreground = new SolidColorBrush(color);
+            menuMutations.Foreground.Opacity = 0.2;
         }
     }
 }
