05aae23911908c5b6711f67bcb402260d0314707
Asklat <algama161098@gmail.com>
2022-01-20 00:17:42 +0100

    Interfaces casi done

--- Desarrollo de interfaces (DEINT)/Segundo bloque/Tema 7/Ejercicios/Actividad21/Actividad21-2/Componentes/PanelHabilidades.xaml.cs
+++ Desarrollo de interfaces (DEINT)/Segundo bloque/Tema 7/Ejercicios/Actividad21/Actividad21-2/Componentes/PanelHabilidades.xaml.cs
@@ -28,12 +28,9 @@ namespace Actividad21_2.Componentes
     {
         private int id;
         private JToken s;
-        private string[] habilidades;
-        private Label[] listaHabilidaes;
 
         public PanelHabilidades(int id)
         {
-            listaHabilidaes = new Label[20];
             this.id = id;
             CargarJson();
             InitializeComponent();
@@ -158,6 +155,8 @@ namespace Actividad21_2.Componentes
                     b.Width = 90;
                     b.Height = 90;
                     b.Margin = new Thickness(24, 5, 0, 5);
+                    b.Cursor = Cursors.Hand;
+                    b.MouseMove += new MouseEventHandler(Drag_MouseMove);
 
                     TextBlock t = new TextBlock();
                     t.Text = (string)s["habilidades"].ElementAt(j);
@@ -172,8 +171,6 @@ namespace Actividad21_2.Componentes
                     lbl.Foreground = new SolidColorBrush(Colors.White);
                     lbl.VerticalContentAlignment = VerticalAlignment.Center;
                     lbl.HorizontalAlignment = HorizontalAlignment.Center;
-                    lbl.Cursor = Cursors.Hand;
-                    lbl.MouseMove += new MouseEventHandler(Drag_MouseMove);
 
                     DropShadowEffect te = new DropShadowEffect();
                     te.ShadowDepth = 3;
@@ -185,7 +182,6 @@ namespace Actividad21_2.Componentes
                     b.Child = lbl;
                     gr.Children.Add(b);
                     j++;
-                    listaHabilidaes.Append(lbl);
                 }
             }
         }
@@ -226,7 +222,7 @@ namespace Actividad21_2.Componentes
                     TextBlock t = new TextBlock();
                     t.Text = (string)s["habilidades"].ElementAt(j);
                     t.TextAlignment = TextAlignment.Center;
-                    t.RenderTransform = new RotateTransform(0, 0, 0);
+                    t.TextWrapping = TextWrapping.Wrap;
                     Panel.SetZIndex(t,1);
                     Label lbl = new Label();
                     lbl.Name = "H" + i;
@@ -237,8 +233,6 @@ namespace Actividad21_2.Componentes
                     lbl.Foreground = new SolidColorBrush(Colors.White);
                     lbl.VerticalContentAlignment = VerticalAlignment.Center;
                     lbl.HorizontalAlignment = HorizontalAlignment.Center;
-                    lbl.Cursor = Cursors.Hand;
-                    lbl.MouseMove += new MouseEventHandler(Drag_MouseMove);
 
                     r.Child = lbl;
                     gm.Children.Add(r);
@@ -250,7 +244,136 @@ namespace Actividad21_2.Componentes
 
         private void HabilidadesP6()
         {
+            List<StackPanel> grupoMutagenos = new List<StackPanel>();
+            grupoMutagenos.Add(GrupoHabilidadesT1);
+            grupoMutagenos.Add(GrupoHabilidadesT2);
+            grupoMutagenos.Add(GrupoHabilidadesT3);
+            grupoMutagenos.Add(GrupoHabilidadesT4);
 
+            int j = 0;
+            int cont = 0;
+            int[] vueltas = {3,2,5,3};
+            foreach (StackPanel gm in grupoMutagenos)
+            {
+                gm.HorizontalAlignment = HorizontalAlignment.Center;
+                gm.Background = null;
+
+                for (int i = 0; i < vueltas[cont]; i++)
+                {
+                    Border r = new Border();
+                    r.Background = PaletaColor(cont, i)[0];
+                    r.BorderBrush = PaletaColor(cont, i)[1];
+                    r.BorderThickness = new Thickness(4);
+                    r.Width = 100;
+                    r.Height = 100;
+                    if (cont == 0)
+                    {
+                        r.Margin = new Thickness(40,10,40,10);
+                    }
+                    else
+                    {
+                        if (cont == 3)
+                        {
+                            r.Margin = new Thickness(70, 10, 70, 10);
+                        }
+                        else
+                        {
+                            r.Margin = new Thickness(10);
+                        }
+                    }
+                    r.CornerRadius = new CornerRadius(50);
+                    r.Cursor = Cursors.Hand;
+                    r.MouseMove += new MouseEventHandler(Drag_MouseMove);
+
+                    TextBlock t = new TextBlock();
+                    t.Text = (string)s["habilidades"].ElementAt(j);
+                    t.TextAlignment = TextAlignment.Center;
+                    t.TextWrapping = TextWrapping.Wrap;
+                    Label lbl = new Label();
+                    lbl.Name = "H" + i;
+                    lbl.Content = t;
+                    lbl.FontSize = 14;
+                    lbl.FontWeight = FontWeights.Bold;
+                    lbl.FontFamily = new FontFamily("Segoe UI");
+                    lbl.Foreground = new SolidColorBrush(Colors.White);
+                    lbl.VerticalContentAlignment = VerticalAlignment.Center;
+                    lbl.HorizontalAlignment = HorizontalAlignment.Center;
+
+                    r.Child = lbl;
+                    gm.Children.Add(r);
+                    j++;
+                }
+                cont++;
+            }
+        }
+
+        private Brush[] PaletaColor(int grupo, int vuelta)
+        {
+            Brush[] r = new Brush[2];
+
+            switch (grupo)
+            {
+                case 0:
+                    if (vuelta == 1)
+                    {
+                        r[0] = Brushes.DarkRed;
+                        r[1] = Brushes.Red;
+                    }
+                    else
+                    {
+                        r[0] = Brushes.Gray;
+                        r[1] = Brushes.White;
+                    }
+                    
+                    break;
+                case 1:
+                    if (vuelta == 0)
+                    {
+                        r[0] = Brushes.DarkViolet;
+                        r[1] = Brushes.Violet;
+                    }
+                    else
+                    {
+                        r[0] = Brushes.YellowGreen;
+                        r[1] = Brushes.Yellow;
+                    }
+                    break;
+                case 2:
+                    if (vuelta < 2)
+                    {
+                        r[0] = Brushes.DarkBlue;
+                        r[1] = Brushes.Blue;
+                    }
+                    if(vuelta == 2)
+                    {
+                        r[0] = Brushes.DarkRed;
+                        r[1] = Brushes.Red;
+                    }
+                    if (vuelta > 2)
+                    {
+                        r[0] = Brushes.DarkGreen;
+                        r[1] = Brushes.Green;
+                    }
+                    break;
+                case 3:
+                    if (vuelta == 0)
+                    {
+                        r[0] = Brushes.DarkViolet;
+                        r[1] = Brushes.Violet;
+                    }
+                    if (vuelta == 1)
+                    {
+                        r[0] = Brushes.Gray;
+                        r[1] = Brushes.White;
+                    }
+                    if (vuelta == 2)
+                    {
+                        r[0] = Brushes.YellowGreen;
+                        r[1] = Brushes.Yellow;
+                    }
+                    break;
+            }
+            return r;
         }
 
         private void Drag_MouseMove(object sender, MouseEventArgs e)
