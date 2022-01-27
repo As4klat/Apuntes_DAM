1354c64bc604420c7ffef8dce811d1acaa15620b
Asklat <dkasalgama@gmail.com>
2022-01-27 09:31:59 +0100

    node terminado

--- Hora de libre configuracion (HLC)/Segundo bloque/Tema 4 - Node.js (2º Trimestre)/Ejercicios/Proyecto 1-Api Json/public/js/main.js
+++ Hora de libre configuracion (HLC)/Segundo bloque/Tema 4 - Node.js (2º Trimestre)/Ejercicios/Proyecto 1-Api Json/public/js/main.js
@@ -1,24 +1,45 @@
 const apiForm = document.querySelector('#apiForm')
 const entrada = document.querySelector('#apiInput')
-const messageOne = document.querySelector('#message-1')
-const messageTwo = document.querySelector('#message-2')
+const loading = document.querySelector('#load')
 
 apiForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
     const location = entrada.value
-
-    messageOne.textContent = 'Loading...'
-    messageTwo.textContent = ''
+    loading.innerHTML = `
+    <div class="spinner-border" role="status">
+        <span class="visually-hidden">Loading...</span>
+    </div>
+    `
 
     fetch('http://localhost:3000/weatherstack?city=' + location).then((response) => {
         response.json().then((data) => {
+            var html=""
             if (data.error) {
-                messageOne.textContent = data.error
+                html = `
+                <div id="card" class="card">
+                    <img src="/img/no-result.png" class="card-img-top" alt="no-result">
+                    <div class="card-body">
+                        <h5 class="card-title">${data.error}</h5>
+                    </div>     
+                </div>
+                `
             } else {
-                messageOne.textContent = data.temperatura
-                console.log(data.temperatura)
+                html = `
+                <div id="card" class="card">
+                    <img src="${data.urlIcon}" class="card-img-top" alt="clima">
+                    <div class="card-img-overlay">
+                        <p class="card-text">Temperatura: ${data.temperatura} ºC</p>
+                        <p class="card-text">Humedad: ${data.humedad}%</p>
+                    </div>
+                    <div class="card-body text-center">
+                        <h5 class="card-title">${location}</h5>
+                        <p class="card-text">${data.descripcionTiempo}</p>
+                        <p class="card-text text-end">${data.tiempoObservacion}</p>
+                    </div>
+                </div>`
             }
+            loading.innerHTML = html
         })
     })
 })
--- Hora de libre configuracion (HLC)/Segundo bloque/Tema 4 - Node.js (2º Trimestre)/Ejercicios/Proyecto 1-Api Json/src/app.js
+++ Hora de libre configuracion (HLC)/Segundo bloque/Tema 4 - Node.js (2º Trimestre)/Ejercicios/Proyecto 1-Api Json/src/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const hbs = require('hbs')
 const postman = require('postman-request')
 const weatherstack = require('../src/utils/weatherstack')
+const { json } = require('express/lib/response')
 
 const app = express()
 
@@ -42,11 +43,27 @@ app.get('/help', (req, res) => {
 app.get('/weatherstack', (req, res) => {
     //http://api.weatherstack.com/current?access_key=&query = New York
     var ciudad = req.query.city
-    postman('http://api.weatherstack.com/current?access_key='+ apikey +'&query='+ciudad, (respuesta, body) => {
+    postman('http://api.weatherstack.com/current?access_key='+ apikey +'&query='+ ciudad, (error, respuesta, body) => {
         if(respuesta.statusCode==200){
             var jsonRespuesta = JSON.parse(body)
+            if(!body.includes("success")){
+                res.send({
+                    tiempoObservacion: jsonRespuesta.current.observation_time,
+                    urlIcon: jsonRespuesta.current.weather_icons,
+                    descripcionTiempo: jsonRespuesta.current.weather_descriptions,
+                    humedad: jsonRespuesta.current.humidity,
+                    temperatura: jsonRespuesta.current.temperature
+                })
+            }
+            else{
+                res.send({
+                    error: "No se ha encontrado su resultado lo sentimos."
+                })
+            }
+            
+        }else{
             res.send({
-                temperatura: jsonRespuesta.current.temperature
+                error: respuesta.statusMessage
             })
         }
     })
