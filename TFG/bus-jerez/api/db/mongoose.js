9eb89c7f10bfa7eeed635972b50ddc0bf7219940
Asklat <algama161098@gmail.com>
2022-04-18 02:03:02 +0200

    tfg

---
+++ TFG/bus-jerez/api/db/mongoose.js
@@ -0,0 +1,3 @@
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb+srv://ra:ra1234@busjerez.hjblk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
---
+++ TFG/bus-jerez/api/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+require('./db/mongoose')
+const userRouter = require('./routers/user')
+const cursoRouter = require('./routers/curso')
+const pruebasRouter = require('./routers/pruebas')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+app.use(userRouter)
+app.use(cursoRouter)
+app.use(pruebasRouter)
+
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
+})
